ff15a7995ed72e1955480acbcc8de8d7bb2c5086
Bakhita Muturi <bakhita.muturi@strathmore.edu>
2025-10-29 13:28:52 +0300

    v=commit three

--- realestate/realestate/settings.py
+++ realestate/realestate/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'properties'
+    'listings'
+    'documents'
+    'enquiries'
+    'accounts'
 ]
 
 MIDDLEWARE = [
